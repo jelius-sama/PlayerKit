5b5e5965773558b9b728a9cd132020806d9eddee
Jelius Basumatary <personal@jelius.dev>
2026-01-14 14:06:58 +0530

    Initial Commit

---
+++ PlayerKit/PlayerKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayerKitApp.swift
+//  PlayerKit
+//
+//  Created by Jelius Basumatary on 14/01/26.
+//
+
+import SwiftUI
+
+@main
+struct PlayerKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
